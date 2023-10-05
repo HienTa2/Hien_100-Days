3daaabfc48b539d51cbb38217e95d9dcae93a00a
Hien Ta <Hienta@PM.ME>
2023-10-05 13:44:03 -0500

    leap year function

--- Day 10 Functions/leapyear.py
+++ Day 10 Functions/leapyear.py
@@ -14,9 +14,13 @@ def is_leap(year):
 
 def days_in_month(year, month):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if month == 2 and is_leap(year):
+        return 29
+    else:
+        return month_days[month - 1]
 
 
-year = int(input("Enter a year."))
-month = int(input("Enter a month."))
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
 days = days_in_month(year, month)
-print(days)
+print(f"Number of days in this month: {days}")
