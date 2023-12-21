0b48176c8bddcd18bdcddc0a23e55eb10608c257
Hien Ta <Hienta@PM.ME>
2023-12-21 16:40:58 -0600

    Day 54 Flask and decorator

--- Day 54 web dev Flask/py_decorator.py
+++ Day 54 web dev Flask/py_decorator.py
@@ -2,18 +2,21 @@ import time
 
 
 def delay_decorator(function):
-    time.sleep(2)
-
     def wrapper_function():
+        time.sleep(2)
         function()
 
-    return wrapper_function()
+    return wrapper_function  # Return the function itself
 
 
+@delay_decorator
 def say_hello():
-    time.sleep(5)
     print("Hello")
 
 
-decorator_function(say_hello)
-say_hello()
+def say_bye():
+    print("Goodbye")
+
+
+say_hello()  # This should now execute with a 2-second delay before printing "Hello"
+say_bye()  # This should print "Goodbye" immediately after "Hello"
